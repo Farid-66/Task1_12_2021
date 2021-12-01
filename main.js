793e2cde31624445c776e0045f7d0869cc1b7727
Farid-66 <feridquba66@gmail.com>
2021-12-01 09:21:31 -0800

    Task

---
+++ main.js
@@ -0,0 +1,95 @@
+// // Task1
+
+// let n=5;
+// let str="";
+// for(let i=0;i<=n;i++){
+//     for(let j=0;j<i;j++){
+//         str=str+"*"
+//     }
+//     str+="\n"
+// }
+
+// console.log(str)
+
+// // -------
+
+
+// // Task2
+
+// let n=1000;
+// let sum=0;
+// for(let i=1;i<=n;i++){
+//     if(i%3==0 && i%5==0){
+//         sum=sum+i;
+//     }
+// }
+// console.log(sum)
+
+// // --------
+
+
+// // Task3
+
+// let num=32243;
+// let str="";
+
+// while(num>0){
+//     let b=num%10;
+//     num=(num-b)/10;
+//     str=str+b
+// }
+// console.log(str)
+
+// // ----------
+
+
+// // Task4
+
+// function Area(a,b){
+//     return a*b;
+// }
+
+// console.log(Area(2,5))
+
+// // --------
+
+
+
+
+// // Task5
+
+
+// function number(a,b){
+//     if(a==b){
+//         return (a+b)*3;
+//     }else{
+//         return a+b;
+//     }
+// }
+
+// console.log(number(5,7))
+
+// // --------
+
+
+
+// // Task6
+
+// let a=-8;
+// let b=9;
+
+
+// if(a<0){
+//     console.log("Negative (a)") 
+// }else{
+//     console.log("Positive (a)")
+// }
+
+// if(b<0){
+//     console.log("Negative (b)") 
+// }
+// else{
+//     console.log("Positive (b)")
+// }
+
+// // -----------
